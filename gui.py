34005c2a26a57f8ddd025e977e11b2169cae0f53
Simone Badaruddin <115610746+simonebadaruddin@users.noreply.github.com>
2024-01-27 15:06:18 -0800

    Update gui.py
    baby amount of code done

--- gui.py
+++ gui.py
@@ -1,26 +1,84 @@
-###
-# i get objects from grade_grapher.py
-#
-#
-#     B  P
-#     Y  R    - a single class (e.g. Math 111) <--- drop down every single class
-#        O    - a single dept (ie math)
-#        F    - single dept + level combo (ie math 400 levels)
-#        
-#      
-#     B  C    - a single dept-level combo (ie math 400 levels) 
-#     Y  L
-#        A
-#        S
-#        S
-# 
-# Dropdowns: All individual classes, all departments, all levels, all instructors or faculty only, by class or by prof 
-#
-# if they leave 
-# so i will get the user's choice of what graphs to display?
-#
-####
+"""
+GUI
+"""
+
+# The graphing library
+import matplotlib.pyplot as plt
+# Used to integrate tkinter & matplotlib + create our canvas
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+# The Graphical User Interface (GUI) Library
 import tkinter as tk
-from tkinter import filedialog # To get the dialog box to open when required 
-from PIL import ImageTk, Image # loading Python Imaging Library
-import json 
+import numpy as np
+
+
+def plot():
+    """
+    Our plotting function gets the selections inputted by the user plus the
+    corresponding data from Grade_grapher.py and plots the resulting graph.
+
+    graph1_title:
+        A single class name, A single department name, a single
+        department name and class level combination, or a single
+        department name plus class level combination
+
+    graph1_x_axis_data:
+
+
+    graph1_y_axis_data:
+        % A's
+
+    graph1_title:
+        A single class name, A single department name, a single
+        department name and class level combination, or a single
+        department name plus class level combination
+
+    ########
+
+    graph2_x_axis_data:
+
+
+    graph2_y_axis_data:
+        % D's / F's
+
+    graph2_title:
+        A single class name, A single department name, a single
+        department name and class level combination, or a single
+        department name plus class level combination
+    """
+    ax.clear()
+    x = np.random.randint(0, 10, 10)  # PLACEHOLDER UNTIL OUR DATA FIXME
+    y = np.random.randint(0, 10, 10)  # PLACEHOLDER UNTIL OUR DATA FIXME
+    ax.plot(x, y)
+    # This call updates the graph:
+    canvas.draw()
+
+
+# Initialize Tkinter
+root = tk.Tk()
+# (a) Create matplotlib figure with access to subplots
+fig, ax = plt.subplots()
+
+# Tkinter Application + Visuals
+frame = tk.Frame(root)
+label = tk.Label(text="Math 111")
+label.config(font=("Courier", 32))
+label.pack()
+
+# Create a canvas which requires:
+# (a) matplotlib figure
+# (b) tkinter application
+canvas = FigureCanvasTkAgg(fig, master=frame)
+# Integrate canvas into the application
+canvas.get_tk_widget().pack()
+
+toolbar = NavigationToolbar2Tk(canvas, frame, pack_toolbar=False)
+toolbar.update()
+toolbar.pack()
+
+# Place label
+frame.pack()
+
+# Create button to plot with prev. plot func, link plotted data here #FIXME
+tk.Button(frame, text="Plot Graph", command=plot).pack(pady=10, side=tk.BOTTOM)
+
+root.mainloop()
