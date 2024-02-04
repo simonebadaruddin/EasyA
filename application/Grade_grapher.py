c26c084a491ed8792381e15ad0aca72d1dd9e584
Luke Marshall <lukemarshall2222@gmail.com>
2024-02-03 20:29:27 -0800

    docstring parse data methods

--- application/Grade_grapher.py
+++ application/Grade_grapher.py
@@ -1,5 +1,21 @@
 """
+Grade_grapher.py
+Original author: Luke Marshall
 Module containing Grapher Python class and its subclasses
+Later additions made by: __________
+
+Contains: 
+Grapher abstract base class: For creating a template for its subclasses, passing down mewthods and attributes
+Courses_By_Prof_Grapher subclass: for graphing individual course data according to instructors
+Subjs_By_Prof_Grapher: for graphing individual subject data according to instructors
+Subjs_And_Level_By_Prof_Grapher: for graphing individual subject and level according to instructors
+Subjs_And_Level_By_Class_Grapher: for graphing individual subject and level according to class
+
+Each class has two main methods along with the regular getter and setter methods:
+parse_data: parses the natty_science_courses attribute in each class into categories and data appropriate to the graph and 
+its options specified by the particular Grapher subclass and the methods used. 
+graph_data: uses the parsed data from the attribute corresponding to user input to graph the grade data 
+according to a category from user input.
 """
 
 
@@ -34,7 +50,7 @@ class Grapher(ABC):
         possible graphs, with the keys being the categories for the graph and the values being the data for each graph, data 
         only references the values for %DsFs
 
-    Methods:
+    Implemented Methods:
         str: for external string representation of the class
         repr: for internal string representation of the class
         get_As_data_all_instructors: Getter method for the As_data_all_instructors attribute
@@ -47,12 +63,8 @@ class Grapher(ABC):
         and filters it for faculty members if the keyword argument faculty_only is set to True
         
     Abstract methods:
-        parse_for_all_instructors: parses the natty_science_course_data attribute into the As_data_all_instructors and 
-        DsFs_data_all_instructors attributes. Parsing is implemented and executed according to the Grapher subclass
-        that it is called on.
-        parse_for_faculty_only: parses the natty_science_course_data attribute into the As_data_faculty_only and
-        DsFs_data_faculty_only attreibutes. Parsing is implemented and executed according to the Grapher subclass 
-        that it is called on.
+        parse_data: parses the natty_science_courses attribute into categories and data appropriate to the graph and its options 
+        specified by the Grapher subclass and the methods used. 
         graph_data: uses the parsed data from the attribute corresponding to user input to graph the grade data 
         according to a category from user input.
         
@@ -646,13 +658,13 @@ class Subjs_And_Level_By_Prof_Grapher(Grapher):
         """Parses the natty_science_courses attribute into categories and data appropriate to the graph and its options 
         specified by the Grapher subclass and the methods used.
         
-        The categories for this parser are grade data in terms of %A or %Ds and %Fs for each of the courses in the natural
-        science subjects for each of the instructors who taught it, along with the number of times they taught it.
-        This is iteratively parsed from the data and represented as a dict with keys that are names of the courses
-        (e.g. MATH111, PSY201) and values that are dicts. The inner dict has the names of all the instructors who taught that 
-        class as keys. The values for the inner dict are a lists, with the first element in each being the total %As or 
-        %Ds and %Fs for all the instances of the class they taught, stored as a float; and the second element being the number 
-        of instances they taught the class  (e.g. if an instructor has taught MATH 111 3 times and the %As were 
+        The categories for this parser are grade data in terms of %A or %Ds and %Fs for each of the course levels in the 
+        natural science subjects for each of the instructors who taught it, along with the number of times they taught it.
+        This is iteratively parsed from the data and represented as a dict with keys that are subjects concatenated with 
+        levels 100-600 (e.g. MATH100, PSY200) and values that are dicts. The inner dict has the names of all the instructors 
+        who taught that class as keys. The values for the inner dict are a lists, with the first element in each being the 
+        total %As or %Ds and %Fs for all the instances of the class they taught, stored as a float; and the second element being 
+        the number of instances they taught the class  (e.g. if an instructor has taught a MATH100 3 times and the %As were 
         45%, 65%, and 55% then the first element in the list will be 110.0, and the second element will be 3). The average 
         percentage of the grades can then be calculated from the two list items by dividing the first element by the second 
         when graphing. 
@@ -873,6 +885,23 @@ class Subjs_And_Level_by_Class_Grapher(Grapher):
     # ================== Data Parsing Method =======================================================================================
 
     def parse_data(self, names_list=[]) -> Dict[str, Dict[str, List[str]]]:
+        """Parses the natty_science_courses attribute into categories and data appropriate to the graph and its options 
+        specified by the Grapher subclass and the methods used.
+        
+        The categories for this parser are grade data in terms of %A or %Ds and %Fs for each of the courses in the natural
+        science subjects, along with the number of times they were taught. This is iteratively parsed from the data and 
+        represented as a dict with keys that are names of the subject code concatenated to a level 100-600 
+        (e.g. MATH100, PSY200) and values that are dicts. The inner dict has the codes of all the classes that were taught in 
+        that subject within that level as keys. The values for the inner dict are a lists, with the first element in each 
+        being the total %As or %Ds and %Fs for all the instances of the class that were taught, stored as a float; and the 
+        second element being the number of instances the class was taught (e.g. if MATH111 has been taught 3 times and the 
+        %As were 45%, 65%, and 55% then the first element in the list will be 110.0, and the second element will be 3). The 
+        average percentage of the grades can then be calculated from the two list items by dividing the first element by the 
+        second when graphing. 
+
+        Returns:
+            The dicts of parsed natty_science_courses data described above
+        """
         # levels (list[str]): a list of the course levels 100 through 600 as strings
         levels = ['100', '200', '300', '400', '500', '600']
         # subj_levels (list[str]): a list of the subjects concatenated with each level in levels
